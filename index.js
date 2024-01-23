e905a9452e79d09977a68cd5e1741c4c609a8687
Chitwan Gandhi <chitwangandhi@Chitwans-MacBook-Air.local>
2024-01-23 14:19:01 +0530

    simple configuration done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express  = require('express');
+const PORT =  process.env.PORT || 3000;
+const colors = require('colors');
+const morgan = require('morgan');
+const dotenv = require("dotenv");
+const app = express();
 
+dotenv.config("");
+
+app.listen(PORT,()=>{
+    console.log(`Server is up!! and running at PORT ${PORT}`.cyan);
+})
